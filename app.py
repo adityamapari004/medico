ab2673497b57bfcd3ac82966fdf3d0fc22b7b689
unknown <adityamapari004@gmail.com>
2026-04-19 18:00:58 +0530

    medigiuide -render deploy

--- app.py
+++ app.py
@@ -109,6 +109,7 @@ def api_predict():
 
 # ── RUN ────────────────────────────────────────
 if __name__ == "__main__":
+    port=int(os.environ.get("PORT",5000))
     app.run(
         host  = "0.0.0.0",
         port  = 5000,
