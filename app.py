b90333d23a810037d253127528691fd869a9035e
unknown <adityamapari004@gmail.com>
2026-05-05 16:12:34 +0530

    port

--- app.py
+++ app.py
@@ -657,9 +657,9 @@ def _chatbot_reply(msg: str) -> str:
 
 # ── RUN ────────────────────────────────────────
 if __name__ == "__main__":
-    port=int(os.environ.get("PORT",5000))
+    port=int(os.environ.get("PORT",10000))
     app.run(
         host  = "0.0.0.0",
-        port  = int(os.environ.get("PORT", 10000)),
+        port  = port
         debug = True
     )
